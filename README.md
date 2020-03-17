9494cc142344900865e9e755262cb8428b39e750
febrian reza <febrianrz@gmail.com>
2020-03-17 23:05:24 +0700

    fixing timeout

--- README.md
+++ README.md
@@ -16,7 +16,7 @@ Notif::send('febrianrz@gmail.com',1,[
 ```
 Parameter pertama adalah email, bisa juga berupa array, parameter kedua adalah id user jika ada, parameter ketiga adalah optionnya.
 
-*Untuk penggunaan via ajax, berikut adalah parameternya:
+* Untuk penggunaan via ajax, berikut adalah parameternya:
 ```json
 app_id:2
 app_secret:VP9u7eIkI9Bsu09AbmfhQIFXGaIeN2ea1YCn4Q7H
--- src/Notif.php
+++ src/Notif.php
@@ -12,29 +12,33 @@ class Notif {
         if(!isset($option['subject'])) dd("Option subject wajib diisi");
         if(!isset($option['body'])) dd("Option body wajib diisi");
 
-        $auth_url = config('micro')['url']['notification'];
-        $client = new Client([
-            'base_uri'  => $auth_url,
-            'timeout'   => 2.0
-        ]);
-        $channel['email'] = is_array($emails)?$emails:[$emails];
-        if($user_id != null){
-            $channel['web'] = [$user_id];
+        try {
+            $auth_url = config('micro')['url']['notification'];
+            $client = new Client([
+                'base_uri'  => $auth_url,
+                'timeout'   => isset(config('micro')['timeout'])?config('micro')['timeout']:10
+            ]);
+            $channel['email'] = is_array($emails)?$emails:[$emails];
+            if($user_id != null){
+                $channel['web'] = [$user_id];
+            }
+            
+            $response = $client->request('POST', '/api/v1/send', [
+                'headers' => ['Accept' => 'application/json'],
+                'form_params'     => [
+                    'app_id'    => config('micro')['app']['id'],
+                    'app_secret'    => config('micro')['app']['secret'],
+                    'channel'   => $channel,
+                    'subject'   => $option['subject'],
+                    'body'      => $option['body']
+                ]
+            ]);
+            
+            if($response->getStatusCode() == 200) return true;
+            return false;
+        } catch(\Exception $e){
+            return false;
         }
-        
-        $response = $client->request('POST', '/api/v1/send', [
-            'headers' => ['Accept' => 'application/json'],
-            'form_params'     => [
-                'app_id'    => config('micro')['app']['id'],
-                'app_secret'    => config('micro')['app']['secret'],
-                'channel'   => $channel,
-                'subject'   => $option['subject'],
-                'body'      => $option['body']
-            ]
-        ]);
-        
-        if($response->getStatusCode() == 200) return true;
-        return false;
     }
 
 }
