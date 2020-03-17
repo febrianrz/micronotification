abe3e276f57b85ea3674356f48eb6b601369382a
febrian reza <febrianrz@gmail.com>
2020-03-17 22:41:47 +0700

    fixing bugs request core

--- README.md
+++ README.md
@@ -3,24 +3,27 @@ Plugin notifikasi untuk framework microservice alter indonesia
 
 ## Installasi
 ```bash
-composer require febrianrz/micromidlleware
+composer require febrianrz/micronotification
 ```
 
 ## Penggunaan
-* Pada file app/Http/Kernel.php pada bagian $routeMiddleware tambahkan
+* Untuk menggunakan library ini, berikut adalah penggunaanya:
 ```bash
-'auth.micro'    => \Febrianrz\Micromidlleware\MicroAuthenticate::class,
-'auth.micro-app'=> \Febrianrz\Micromidlleware\MicroAppAuthenticate::class,
+Notif::send('febrianrz@gmail.com',1,[
+    'subject'   => 'Subjeknya',
+    'body'      => 'Bodynya'
+]);
 ```
-* Pada bagian api.php, gunakan middleware 
-```bash
-Route::middleware('auth.micro')
-```
-* Pada setiap request, data user dapat digunakan dengan cara
-```bash
-$request->user
-```
-* Lakukan publish configurasi dengan perintah
-```bash
-php artisan vendor:publish
+Parameter pertama adalah email, bisa juga berupa array, parameter kedua adalah id user jika ada, parameter ketiga adalah optionnya.
+
+*Untuk penggunaan via ajax, berikut adalah parameternya:
+```json
+app_id:2
+app_secret:VP9u7eIkI9Bsu09AbmfhQIFXGaIeN2ea1YCn4Q7H
+subject:Subjeknya
+body:tes
+channel[email][]:febrianrz@gmail.com
+channel[email][]:admin@admin.com
+channel[telegram][]:channel id
+channel[web][]:link
 ```
--- src/Notif.php
+++ src/Notif.php
@@ -8,23 +8,33 @@ class Notif {
 
     public static function send($to,$option)
     {
+        if(!is_array($option)) dd("Option harus berupa array");
+        if(!isset($option['subject'])) dd("Option subject wajib diisi");
+        if(!isset($option['body'])) dd("Option body wajib diisi");
+
         $auth_url = config('micro')['url']['notification'];
-        // dd($auth_url);
         $client = new Client([
             'base_uri'  => $auth_url,
             'timeout'   => 2.0
         ]);
+        $channel['email'] = is_array($emails)?$emails:[$emails];
+        if($user_id != null){
+            $channel['web'] = [$user_id];
+        }
+        
         $response = $client->request('POST', '/api/v1/send', [
+            'headers' => ['Accept' => 'application/json'],
             'form_params'     => [
                 'app_id'    => config('micro')['app']['id'],
-                'app_secret'    => config('micro')['app']['id'],
+                'app_secret'    => config('micro')['app']['secret'],
+                'channel'   => $channel,
+                'subject'   => $option['subject'],
+                'body'      => $option['body']
             ]
         ]);
-        dd($response->getStatusCode());
-        // if($response->getStatusCode() == 200){
-        //     dd("berhasil");
-        // } 
-        // dd("gagal");
+        
+        if($response->getStatusCode() == 200) return true;
+        return false;
     }
 
 }
